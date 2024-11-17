e263d152b527ea17c84bdf10b8375cf8871e626d
Alex Zhuravlev <leftemail17@mail.ru>
2024-11-17 13:21:11 +0300

    feat(HW-5): add statik for swagger

--- internal/client/kafka/kafka.go
+++ internal/client/kafka/kafka.go
@@ -2,15 +2,7 @@ package kafka
 
 import (
 	"context"
-
-	"github.com/greenblat17/auth/internal/client/kafka/consumer"
 )
-
-// Consumer represents kafka consumer
-type Consumer interface {
-	Consume(ctx context.Context, topicName string, handler consumer.Handler) (err error)
-	Close() error
-}
 
 // Producer represents kafka producer
 type Producer interface {
