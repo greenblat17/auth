2d356db697175cd8319d904e9a890cbcdc30d31c
Alex Zhuravlev <leftemail17@mail.ru>
2024-11-18 18:16:05 +0300

    feat(HW-5): add comment for ValidateInterceptor

--- internal/interceptor/validate.go
+++ internal/interceptor/validate.go
@@ -10,6 +10,7 @@ type validator interface {
 	Validate() error
 }
 
+// ValidateInterceptor s a gRPC server interceptor that validates incoming requests
 func ValidateInterceptor(ctx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	if val, ok := req.(validator); ok {
 		if err := val.Validate(); err != nil {
